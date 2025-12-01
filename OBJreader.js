5913eed33649f9f8b077d2f3c6dd5f7edac3f3e9
FireDude123 <153016828+F1reDude123@users.noreply.github.com>
2025-12-01 12:46:31 -0500

    Update OBJreader.js

--- OBJreader.js
+++ OBJreader.js
@@ -33,4 +33,37 @@ export default class OBJreader {
       getInds: function() { return inds; }
     };
   }
+  static async readURL(u) {
+    var url = fetch(u).then(r => r.text()).then(t => {
+      var verts = [];
+      var inds = [];
+      t.split("\n").forEach(function(e) {
+        switch (e[0]) {
+          case "v":
+            var points = e.substring(2, e.length).split(" ");
+            verts.push(new Vector3(parseFloat(points[0]), parseFloat(points[1]), parseFloat(points[2])));
+            break;
+          case "f":
+            var points = e.substring(2, e.length).split(" ");
+            if (points.length == 3) {
+              inds.push(parseInt(points[0])-1);
+              inds.push(parseInt(points[1])-1);
+              inds.push(parseInt(points[2])-1);
+            }
+            else {
+              inds.push(parseInt(points[0])-1);
+              inds.push(parseInt(points[1])-1);
+              inds.push(parseInt(points[2])-1);
+              inds.push(parseInt(points[2])-1);
+              inds.push(parseInt(points[3])-1);
+              inds.push(parseInt(points[0])-1);
+            }
+        }
+      });
+      return {
+        getVerts: function() { return verts; },
+        getInds: function() { return inds; }
+      };
+    });
+  }
 }
