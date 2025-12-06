302667e7e207cf00f19132a755a26a037584af29
FireDude123 <153016828+F1reDude123@users.noreply.github.com>
2025-12-05 19:36:54 -0500

    Update Polygon.js

--- Polygon.js
+++ Polygon.js
@@ -1,5 +1,7 @@
+import Vector3 from "https://f1redude123.github.io/Vecter/Vector3.js";
+
 export default class Polygon {
-  normal;
+  normal = new Vector3(0, 0, 0);
   constructor(p1, p2, p3) {
     [this.p1, this.p2, this.p3] = [p1, p2, p3];
   }
